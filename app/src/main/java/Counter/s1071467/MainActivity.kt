88c5e3de189daadf687dc81b1ee733be2158deb7
student <s1071467@gm.pu.edu.tw>
2021-03-26 15:54:29 +0800

    新增圖片0326

--- app/src/main/java/Counter/s1071467/MainActivity.kt
+++ app/src/main/java/Counter/s1071467/MainActivity.kt
@@ -7,22 +7,50 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(),
+        View.OnClickListener,View.OnLongClickListener{
     var counter:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+        txv.setOnLongClickListener(this)
+        img.setOnLongClickListener(this)
+        btn.setOnClickListener(this)
+        btn2.setOnClickListener(this)
+        btn3.setOnClickListener(this)
+        btn4.setOnClickListener(this)
+        btnHappy.setOnClickListener(this)
 
     }
 
-        fun add(v:View) {
-         if(v==btn){
-             counter++
-         }else if(v==btn2){
-             counter+=2
-         }
-         txv.text=counter.toString()
 
-     }
+
+    override fun onClick(v: View?) {
+        if(v==txv || v==btn) {
+            counter++
+        }else if(v==btn2){
+            counter+=2
+        }else if(v==btn3){
+            counter=0
+        }else if(v==btn4){
+            System.exit(0)
+        }else if(v==btnHappy){
+            counter=(1..100).random()
+
+        }
+        txv.text=counter.toString()
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        if(v==txv) {
+            counter += 2
+        }else if(v==img){
+            counter--
+        }
+        txv.text=counter.toString()
+        return true
+
+    }
 
 }
